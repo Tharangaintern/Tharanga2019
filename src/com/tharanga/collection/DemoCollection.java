02a98a3924ae33996e7b668e894f76bba54064c4
Shivani Sura <shivanisura31@gmail.com>
2019-06-26 14:59:00 -0700

    first commit

--- src/com/tharanga/collection/DemoCollection.java
+++
@@ -1,8 +0,0 @@
-package com.tharanga.collection;
-
-public class DemoCollection {
-
-	public void check() {
-		System.out.println("inside a check");
-	}
-}
--- src/com/tharanga/io/Demoio.java
+++
@@ -1,8 +0,0 @@
-package com.tharanga.io;
-
-public class Demoio {
-
-	public void checkIo() {
-		System.out.println("inside a check method");
-	}
-}
