533239d8ffe6a17f38a220fffea68e70706d2ae5
snehashreeMN <snehatharangatechnologies@gmail.com>
2019-06-27 16:32:08 +0530

    CRUD_delete operation

---
+++ javaexamples/CRUD_DeleteOperation/src/com/javatpoint/mypackage/StoreData.java
@@ -0,0 +1,34 @@
+package com.javatpoint.mypackage;  
+  
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.Transaction;  
+import org.hibernate.boot.Metadata;  
+import org.hibernate.boot.MetadataSources;  
+import org.hibernate.boot.registry.StandardServiceRegistry;  
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
+  
+public class StoreData {  
+  
+    public static void main( String[] args )  
+    {  
+         StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+            Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+          
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+        Session session = factory.openSession();  
+        Transaction t = session.beginTransaction();  
+          
+         Employee e1=new Employee();    
+            e1.setId(3);    
+            //e1.setFirstName("suman");    
+            //e1.setLastName("Chawla");    
+         
+       //session.save(e1);  
+            session.delete(e1);
+       t.commit();  
+       System.out.println("successfully deleted");    
+        factory.close();  
+        session.close();     
+    }  
+}  
